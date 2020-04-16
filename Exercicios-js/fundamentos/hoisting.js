f36069ae410abdb1aa48f7df9d807141591631c1
pameladilly <pameladilly@hotmail.com>
2020-04-16 08:11:47 -0300

    Let, laçoes e funções

---
+++ Exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,19 @@
+console.log('a =', a)
+
+var a = 2
+
+console.log('a =', a)
+
+function teste() {
+    console.log('a =', a)
+
+    var a = 3
+
+    console.log('a =', a)
+}
+
+teste()
+
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
