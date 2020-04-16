6df5503df3a559e060aac44d48d6d2f165925be3
pameladilly <pameladilly@hotmail.com>
2020-04-16 07:06:06 -0300

    Funções e variáveis

---
+++ Exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,19 @@
+//funcao sem retorno
+
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 3, 4, 5)
+imprimirSoma()
+
+//Funcao com retorno
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(7))
+console.log(soma())
