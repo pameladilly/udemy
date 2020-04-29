a26575d108cdaa0156079a965e066832bc8c4be9
pameladilly <pameladilly@hotmail.com>
2020-04-29 05:45:33 -0300

    Exercícios

--- Exercicios-js/lista de exercicios/exercicio1.js
+++ Exercicios-js/lista de exercicios/exercicio1.js
@@ -1,1 +1,24 @@
-sadsd
+/*01) Crie uma função que dado dois valores(passados como parâmetros) mostre no console a soma, subtração,
+    multiplicação e divisão desses valores
+*/
+
+function somar(v1, v2) {
+    console.log(v1 + v2)
+}
+
+const subtrair = (v1, v2) => {
+    console.log(v1 - v2)
+}
+
+const multiplicar = (v1, v2) => {
+    console.log(v1 * v2)
+}
+
+const dividir = (v1, v2) => {
+    console.log(v1 / v2)
+}
+
+somar(1, 5)
+subtrair(10, 9)
+multiplicar(5, 5)
+dividir(10, 5)
---
+++ Exercicios-js/lista de exercicios/exercicio3.js
@@ -0,0 +1,6 @@
+/*03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente. */
+baseElevadaAoExpooente = (base, expoente) => {
+    return Math.pow(base, expoente)
+}
+
+console.log(baseElevadaAoExpooente(2, 3))
