f609f567065ad2d4d09951885bfaaa06f2f659e2
pameladilly <pameladilly@hotmail.com>
2020-05-01 05:12:32 -0300

    Aula do dia 30/04

---
+++ Exercicios-js/objeto/objetosConstantes.js
@@ -0,0 +1,17 @@
+// pessoa -> 123 {...}
+const pessoa = { nome: 'João' }
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+//pessoa -> 456 -> {...}
+//pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+pessoa.nome = 'Maria'
+pessoa.rua = 'Rua ABC'
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({ nome: 'João' })
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
