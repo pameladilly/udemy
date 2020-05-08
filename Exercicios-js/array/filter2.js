7e95041edaaaf725669f24128cd1e4f051c1ff73
pameladilly <pameladilly@hotmail.com>
2020-05-08 06:59:20 -0300

    aula dia 08-05

---
+++ Exercicios-js/array/filter2.js
@@ -0,0 +1,26 @@
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const frageis =
+    function(produto) {
+        return (produto.fragil) ? true : false
+    }
+
+const caros =
+    (produto) => produto.preco >= 2500
+
+console.log(produtos.filter2(frageis).filter2(caros))
